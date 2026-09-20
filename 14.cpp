e50af827cdec3fa26206d1a8716a605fa66f2d9f
BP5751 <bcporteous5@gmail.com>
2026-09-20 14:00:31 +1200

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string prefix = strs[0];
+
+        if (strs.empty()) {
+            return "";
+        }
+
+        for(int i=1;i<strs.size();i++){
+            while (strs[i].find(prefix) !=0){
+                prefix.pop_back();
+            }
+        }
+
+        return prefix;
+    }
+};
